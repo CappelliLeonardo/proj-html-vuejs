bd8f8eb6ca46a3bcf67b3fa52643219f46e327cb
CappelliLeonardo <l.cappelli1995@gmail.com>
2024-01-29 19:23:09 +0100

    Contact Us section WIP

--- src/components/ContactUsSection.vue
+++ src/components/ContactUsSection.vue
@@ -14,16 +14,56 @@ export default {
 <template>
     <main>
         <div class="container-fluid">
-            <div class="container">
+            <div class="container py-5 ">
                 <h1>
                     CONTACT US
                 </h1>
                 <div>
-                    <a href="https://wordpressthemes.live/WP01/WP015/">
-                        Home
-                    </a>
-                    /
-                    Contact Us
+                    <a href="https://wordpressthemes.live/WP01/WP015/">Home</a>
+                    <span> /</span>
+                    <span> Contact Us</span>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        
+                        <form class="w-100">
+                            <div class="row">
+                                <div class="col">
+                                    <label for="name">Your Name</label>
+                                    <input type="text" id="name" class="form-control">
+                                </div>
+                                <div class="col">
+                                    <label for="email">Your Email</label>
+                                    <input type="text" id="email" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <label for="subject">Subject</label>
+                                    <input type="text" id="subject" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <label for="text">Your Message (Optional)</label>
+                                    <div class="w-100">
+                                        <textarea class="text-area" name="" id="text" cols="" rows="1">
+
+                                        </textarea>
+                                    </div>
+                                        
+                                  
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-6">
+                        SEZIONE GET IN TOUCH
+                    </div>
                 </div>
             </div>
         </div>
@@ -31,5 +71,21 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-
+.container-fluid{
+    background-color: #121212;
+    color: white;
+    text-align: center;
+    .container{
+        
+        a{
+            text-decoration: underline;
+            &:hover{
+                color: #F2870C;
+            }
+        }
+    }
+}
+.text-area{
+    resize: both;
+}
 </style>
