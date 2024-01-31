728688dea3ebfbbd6e9e3851d557804aa2b7908e
VincentDS98 <disanto.vincenzo98@gmail.com>
2024-01-31 11:18:55 +0100

    counter/parallax for about

--- src/components/AboutUsSection.vue
+++ src/components/AboutUsSection.vue
@@ -1,9 +1,15 @@
 <script>
+import ParallaxForAbout from './ParallaxForAbout.vue';
+import CounterSection from './CounterSection.vue';
 export default {
     data() {
         return {
 
         };
+    },
+    components: {
+        ParallaxForAbout,
+        CounterSection
     },
     methods: {
 
@@ -52,11 +58,10 @@ export default {
                 <div class="boxImg">
                     <img src="https://www.rockshots.eu/wp-content/uploads/2018/02/jesse-ramirez-R1NBUvxIdu8-unsplash-960x960.jpg" alt="">
                 </div>
-            </div>
-
-            
-           
+            </div>  
         </section>
+        <ParallaxForAbout/>
+        
         <section  class="container-fluid">
                     <div class="container">
                         <div>
@@ -100,12 +105,19 @@ export default {
                             
                         
 
-            </section>
+        </section>
+
+        <CounterSection/>
+
+        <section>
+            
+        </section>
 
     </main>
 </template>
 
 <style lang="scss" scoped>
+
     .container-fluid{
         
         background-color: black;
@@ -160,5 +172,18 @@ export default {
         max-width: 100%;
         max-height: 100%;
         
+    }
+
+    button{
+    background-color: transparent;
+    color: white;
+    border-color: #F2870C;
+    font-size: 15px;
+    padding: 10px 50px;
+    margin-top: 35px;
+    }
+
+    button:hover{
+    background-color:#F2870C ;
     }
 </style>
---
+++ src/components/ParallaxForAbout.vue
@@ -0,0 +1,79 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+    components:{
+        
+    }
+}
+</script>
+
+<template>
+    <div class="container-fluid parallax-container">
+        <div class="text-container text-center ">
+            <h4 class="mb-2 active">INSTRUMENTAL ROCK</h4>
+            <p class="mb-3 album">ALBUM OUT NOW</p>
+            <button>
+                <a href="#">READ MORE</a>
+            </button>
+        </div>
+    </div>
+</template>
+                               
+                               
+
+<style lang="scss" scoped>
+@use "../assets/scss/main.scss" as *;
+.parallax-container{
+    height:800px;
+    background-image: url('../../public/instrumental-rock/assets/cms_parallax.jpg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    background-attachment: fixed;
+    .text-container{
+        margin: 0 auto;
+        width: 1400px;
+        height: 100%;
+        color: white;
+        padding-top: 250px;
+        button:hover{
+            background-color:#F2870C ;
+        } 
+         button{
+                width: 200px;
+                height: 50px;
+                background-color: transparent;
+                border-color: #F2870C;
+                font-size: 15px;
+                 a{
+                    
+                    text-decoration: none;
+                    color: white;
+                    }
+               }
+               .album{
+                font-size: 5.5rem;
+                font-weight: 500;
+                
+               }
+    }   
+}
+
+.active {
+    color: orange;
+}
+
+
+
+    button:hover{
+    background-color:#F2870C ;
+    }
+</style>
